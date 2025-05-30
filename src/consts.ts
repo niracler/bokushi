d92bbdb7eae54c91d2cbbd08a46dbbb4f1341b07
niracler <i@niracler.com>
2025-05-30 16:39:35 +0800

    feat(footer): add social links with custom tooltip styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'niracler';
+export const SITE_DESCRIPTION = '长门大明神会梦到外星羊么？';
+
+// 社交媒体链接
+export const SOCIAL_LINKS = {
+    github: 'https://github.com/niracler',
+    twitter: 'https://x.com/nini38324',
+    email: 'mailto:i@niracler.com',
+    telegram: 'https://t.me/niracler'
+};
