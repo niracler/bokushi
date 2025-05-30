b3d1abbf8b3508a48fcaf3195383b5a5eef76e34
niracler <i@niracler.com>
2025-05-30 21:02:09 +0800

    Update social links and site branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Niracler 的博物志';
+export const SITE_TITLE = '博物志';
 export const SITE_DESCRIPTION = '长门大明神会梦到外星羊么？';
 
 // 社交媒体链接
@@ -9,5 +9,5 @@ export const SOCIAL_LINKS = {
     github: 'https://github.com/niracler',
     twitter: 'https://x.com/nini38324',
     email: 'mailto:i@niracler.com',
-    telegram: 'https://t.me/niracler'
+    telegram: 'https://t.me/tomoko_channel'
 };
