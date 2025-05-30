3b56e2e3e4ac067703d8558f19a8e3ca051fc532
niracler <i@niracler.com>
2025-05-30 15:51:10 +0800

    feat(blog): add Chinese font and update social links
        • Added `@fontsource/lxgw-wenkai` for Chinese typography support
        • Updated social media links to personal accounts (X and GitHub)
        • Changed site branding to 'Niracler'
        • Modified RSS feed title and link
        • Applied LXGW WenKai font in global CSS
        • Removed Mastodon link and updated Twitter to X
        • Updated footer copyright to reflect personal branding

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
@@ -8,9 +12,13 @@ export async function GET(context) {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
+		trailingSlash: false,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/${post.id}/`,
+			content: sanitizeHtml(parser.render(post.body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+			}),
 		})),
 	});
 }
