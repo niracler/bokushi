e5a3b29f7162f311b5812eb174eaf9639adfe846
niracler <i@niracler.com>
2025-05-31 15:04:41 +0800

    feat(astro): remove trailing slashes in URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://niracler.com',
+  trailingSlash: 'never',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
   markdown: {
