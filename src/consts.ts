a5bf642e823ef887b333ba57e57f49b78b5fe7f2
niracler <i@niracler.com>
2025-11-05 18:18:01 +0800

    feat: refresh navigation and social meta

--- src/consts.ts
+++ src/consts.ts
@@ -3,12 +3,16 @@
 
 export const SITE_TITLE = "Niracler 的博物志";
 export const SITE_DESCRIPTION = "长门大明神会梦到外星羊么？";
+export const SITE_SOCIAL_IMAGE =
+  "https://image.niracler.com/2025/11/310d7fe6f129e0beae659033fbba30a8.jpeg";
 
 // 社交媒体链接
 export const SOCIAL_LINKS = {
-    github: "https://github.com/niracler",
-    twitter: "https://x.com/nini38324",
-    email: "mailto:i@niracler.com",
-    telegram: "https://t.me/tomoko_channel",
-    rss: "/rss.xml",
+  github: "https://github.com/niracler",
+  twitter: "https://x.com/nini38324",
+  email: "mailto:i@niracler.com",
+  telegram: "https://t.me/tomoko_channel",
+  steam: "https://steamcommunity.com/id/niracler/",
+  douban: "https://www.douban.com/people/niracler/",
+  rss: "/rss.xml",
 };
